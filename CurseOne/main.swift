423719b488692aa42ce16cbaa57ba0f8f043c509
Slip_42 <danil.daba4300@gmail.com>
2022-01-21 08:28:51 +0200

    Домашнее задание №6 выполнено + тест Гита

--- CurseOne/main.swift
+++ CurseOne/main.swift
@@ -7,6 +7,59 @@
 
 import Foundation
 
-print("Hello, World!")
+print("Hello, Teacher!")
 
-let car = "BMW"
+protocol Farm {
+     var animalcCount: Int { get set }
+ }
+
+ enum animalList {
+     case cow
+     case pig
+     case hen
+     case cock
+     case horse
+ }
+
+ struct Animal: Farm {
+     var animalcCount: Int
+     var animalAmount: animalList
+ }
+
+ extension Animal: CustomStringConvertible {
+     var description: String {
+         return "There are \(animalcCount) of \(animalAmount) on the farm"
+     }
+ }
+
+ struct AnimalTwo <T: Farm> {
+     private var animals: [T] = []
+
+     mutating func add(animal: T) {
+         animals.append(animal)
+     }
+
+     mutating func removeFirst() {
+         animals.removeFirst()
+     }
+
+     mutating func removeLast() {
+         animals.removeLast()
+     }
+
+     mutating func removeElement(position: Int)  {
+         if position < animals.count {
+             animals.remove(at: position)
+         } else { print("There's no \(position) position in this queue.") }
+     }
+ }
+
+ extension AnimalTwo {
+     func one (chosenCondition: (T) -> Bool) -> AnimalTwo<T> {
+         var newAnimal = AnimalTwo<T>()
+         for value in animals where chosenCondition(value) {
+             newAnimal.add(animal: value)
+         }
+         return newAnimal
+     }
+ }
